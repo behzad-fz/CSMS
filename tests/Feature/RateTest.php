daf94db648bab2c49b97d8e43193cfab7861dfb4
BehzadFazelasl <behzadfazelasl@teknasyon.com>
2022-03-07 16:55:40 +0300

    Add get error if given meter count is invalid

--- tests/Feature/RateTest.php
+++ tests/Feature/RateTest.php
@@ -88,7 +88,7 @@ class RateTest extends TestCase
                 "meterStart" => 1204307,
                 "timestampStart" => "2021-04-05T10:04:00Z",
                 "meterStop" => 1215230,
-                "timestampStop" => "2020-04-05T11:27:00Z"
+                "timestampStop" => "2000-01-01T00:00:01Z"
             ]
         ];
 
@@ -99,6 +99,39 @@ class RateTest extends TestCase
                  'errors' => [
                      "cdr.timestampStop" => [
                          "The cdr.timestamp stop must be a date after or equal to cdr.timestamp start."
+                     ],
+                 ]
+             ]);
+    }
+
+    /**
+     *  test user gets validation error with 422 status code if input start meter is greater than end meter.
+     *
+     * @return void
+     */
+    public function test_user_gets_validation_error_if_start_meter_is_greater_than_end_meter()
+    {
+        $input = [
+            "rate" => [
+                "energy" => 0.3,
+                "time" => 2,
+                "transaction"=>  1
+            ],
+            "cdr" => [
+                "meterStart" => 1204307,
+                "timestampStart" => "2021-04-05T10:04:00Z",
+                "meterStop" => 1,
+                "timestampStop" => "2021-04-05T11:27:00Z"
+            ]
+        ];
+
+        $this->json('POST','/rate', $input)
+            ->assertStatus(422)
+            ->assertJson([
+                 'message' => "The cdr.meter stop must be greater than 1204307.",
+                 'errors' => [
+                     "cdr.meterStop" => [
+                         "The cdr.meter stop must be greater than 1204307."
                      ],
                  ]
              ]);
